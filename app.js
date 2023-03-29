ff9789f4fb1b04a8bacc94e4be9583cf28b04e02
Ömer <omergungorco@gmail.com>
2023-03-29 13:56:41 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -2,16 +2,30 @@ const express = require('express');
 
 const app = express();
 
+const loginCheck = (req, res, next) => {
+    let isLogin = true;
+
+    if (isLogin) {
+        next();
+    } else {
+        res.send('You are not logged in');
+    }
+}
+
+app.use(express.static('public'));
+app.use(loginCheck);
+
 app.get('/', (req, res) => {
     const photo = {
         id: 1,
-        photoName: "Photo Name",
-        description: "Photo Description",
-    }
-    res.send(photo);
+        photoName: 'Photo Name',
+        description: 'Photo Description',
+    };
+    // res.send(photo);
+    res.sendFile(__dirname + '/temp/index.html');
 });
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
